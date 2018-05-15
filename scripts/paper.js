dafa78d2b6b65f4423ed4643379de76ab78c4e1b
Terence Yu <rencete@yahoo.com>
2018-05-15 22:09:10 +0800

    Animation of circle to get smaller until it gets removed

--- scripts/paper.js
+++ scripts/paper.js
@@ -1,5 +1,34 @@
+// Make a symbol of a circle
+var base = Shape.Circle(view.center, 60)
+base.fillColor = 'black'
+var symbol = new Symbol(base);
+
 function onKeyDown(event) {
-	var shape = new Shape.Circle(view.viewSize * Point.random(), 30);
-    shape.strokeColor = 'black';
-    return false;
+    // Place circle in random location
+    symbol.place(view.viewSize * Point.random())
+
+    // Select last placed symbol
+    var ps = project.activeLayer.lastChild
+    // Randomize the size of the circle
+    var newRadius = Math.random() * (ps.bounds.width) + 60
+    ps.bounds.width = newRadius
+    ps.bounds.height = ps.bounds.width
+    return false
+}
+
+
+
+// Reduce the size until it's gone
+function onFrame(event) {    
+	for (var i = 0; i < project.activeLayer.children.length; i++) {
+		var item = project.activeLayer.children[i]
+        
+        // Reduce the size
+        item.bounds.width -= 3
+        item.bounds.height -= 3
+
+		if (item.bounds.width <= 3) {
+			item.remove()
+		}
+	}
 }
